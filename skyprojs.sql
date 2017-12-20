5f502dcb71ae5aad94d1a9b2ec12999fa541e4f1
Anton <attim38@gmail.com>
2017-12-20 22:13:08 +0700

    Исправлен поиск

--- skyprojs.sql
+++ skyprojs.sql
@@ -1,17 +1,17 @@
 /*
 Navicat PGSQL Data Transfer
 
-Source Server         : localhost_5432
-Source Server Version : 90503
+Source Server         : postgreSQL
+Source Server Version : 90603
 Source Host           : localhost:5432
 Source Database       : skyprojs
 Source Schema         : public
 
 Target Server Type    : PGSQL
-Target Server Version : 90503
+Target Server Version : 90603
 File Encoding         : 65001
 
-Date: 2017-11-23 01:43:48
+Date: 2017-12-20 22:12:21
 */
 
 
@@ -35,9 +35,9 @@ CREATE SEQUENCE "public"."direction_id_seq"
  INCREMENT 1
  MINVALUE 1
  MAXVALUE 9223372036854775807
- START 3
+ START 30
  CACHE 1;
-SELECT setval('"public"."direction_id_seq"', 3, true);
+SELECT setval('"public"."direction_id_seq"', 30, true);
 
 -- ----------------------------
 -- Sequence structure for grnti_id_seq
@@ -47,9 +47,9 @@ CREATE SEQUENCE "public"."grnti_id_seq"
  INCREMENT 1
  MINVALUE 1
  MAXVALUE 9223372036854775807
- START 8
+ START 9
  CACHE 1;
-SELECT setval('"public"."grnti_id_seq"', 8, true);
+SELECT setval('"public"."grnti_id_seq"', 9, true);
 
 -- ----------------------------
 -- Sequence structure for project_id_seq
@@ -129,7 +129,7 @@ WITH (OIDS=FALSE)
 -- Records of classificate_2
 -- ----------------------------
 INSERT INTO "public"."classificate_2" VALUES ('1', '9');
-INSERT INTO "public"."classificate_2" VALUES ('10', '13');
+INSERT INTO "public"."classificate_2" VALUES ('10', '9');
 
 -- ----------------------------
 -- Table structure for classificate_3
@@ -146,8 +146,6 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 -- Records of classificate_3
 -- ----------------------------
-INSERT INTO "public"."classificate_3" VALUES ('3', '1');
-INSERT INTO "public"."classificate_3" VALUES ('3', '10');
 
 -- ----------------------------
 -- Table structure for collaborator
@@ -200,7 +198,33 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 -- Records of directions
 -- ----------------------------
-INSERT INTO "public"."directions" VALUES ('3', 'Информационные технологии');
+INSERT INTO "public"."directions" VALUES ('4', 'Базовые и критические военные и промышленные технологии для создания перспективных видов вооружения, военной и специальной техники');
+INSERT INTO "public"."directions" VALUES ('5', 'Базовые технологии силовой электротехники');
+INSERT INTO "public"."directions" VALUES ('6', 'Биокаталитические, биосинтетические и биосенсорные технологии');
+INSERT INTO "public"."directions" VALUES ('7', 'Биомедицинские и ветеринарные технологии');
+INSERT INTO "public"."directions" VALUES ('8', 'Геномные, протеомные и постгеномные технологии');
+INSERT INTO "public"."directions" VALUES ('9', 'Клеточные технологии');
+INSERT INTO "public"."directions" VALUES ('10', 'Компьютерное моделирование наноматериалов, наноустройств и нанотехнологий');
+INSERT INTO "public"."directions" VALUES ('11', 'Нано-, био-, информационные, когнитивные технологии');
+INSERT INTO "public"."directions" VALUES ('12', 'Технологии атомной энергетики, ядерного топливного цикла, безопасного обращения с радиоактивными отходами и отработавшим ядерным топливом');
+INSERT INTO "public"."directions" VALUES ('13', 'Технологии биоинженерии');
+INSERT INTO "public"."directions" VALUES ('14', 'Технологии диагностики наноматериалов и наноустройств');
+INSERT INTO "public"."directions" VALUES ('15', 'Технологии доступа к широкополосным мультимедийным услугам');
+INSERT INTO "public"."directions" VALUES ('16', 'Технологии информационных, управляющих, навигационных систем');
+INSERT INTO "public"."directions" VALUES ('17', 'Технологии наноустройств и микросистемной техники');
+INSERT INTO "public"."directions" VALUES ('18', 'Технологии новых и возобновляемых источников энергии, включая водородную энергетику');
+INSERT INTO "public"."directions" VALUES ('19', 'Технологии получения и обработки конструкционных наноматериалов');
+INSERT INTO "public"."directions" VALUES ('20', 'Технологии получения и обработки функциональных наноматериалов');
+INSERT INTO "public"."directions" VALUES ('21', 'Технологии и программное обеспечение распределенных и высокопроизводительных вычислительных систем');
+INSERT INTO "public"."directions" VALUES ('22', 'Технологии мониторинга и прогнозирования состояния окружающей среды, предотвращения и ликвидации ее загрязнения');
+INSERT INTO "public"."directions" VALUES ('23', 'Технологии поиска, разведки, разработки месторождений полезных ископаемых и их добычи');
+INSERT INTO "public"."directions" VALUES ('24', 'Технологии предупреждения и ликвидации чрезвычайных ситуаций природного и техногенного характера');
+INSERT INTO "public"."directions" VALUES ('25', 'Технологии снижения потерь от социально значимых заболеваний');
+INSERT INTO "public"."directions" VALUES ('26', 'Технологии создания высокоскоростных транспортных средств и интеллектуальных систем управления новыми видами транспорта');
+INSERT INTO "public"."directions" VALUES ('27', 'Технологии создания ракетно-космической и транспортной техники нового поколения');
+INSERT INTO "public"."directions" VALUES ('28', 'Технологии создания электронной компонентной базы и энергоэффективных световых устройств');
+INSERT INTO "public"."directions" VALUES ('29', 'Технологии создания энергосберегающих систем транспортировки, распределения и использования энергии');
+INSERT INTO "public"."directions" VALUES ('30', 'Технологии энергоэффективного производства и преобразования энергии на органическом топливе');
 
 -- ----------------------------
 -- Table structure for grnti
@@ -219,9 +243,10 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 -- Records of grnti
 -- ----------------------------
-INSERT INTO "public"."grnti" VALUES ('6', '20.53.17', 'Тест', '8');
-INSERT INTO "public"."grnti" VALUES ('7', '20.53.18', 'asd', '8');
-INSERT INTO "public"."grnti" VALUES ('8', '20.53', 'аа', null);
+INSERT INTO "public"."grnti" VALUES ('6', '20.53.17', 'Средства хранения информации', '8');
+INSERT INTO "public"."grnti" VALUES ('7', '20.53.19', 'Средства обработки и поиска информации', '8');
+INSERT INTO "public"."grnti" VALUES ('8', '20.53', 'Технические средства обеспечения информационных процессов', '9');
+INSERT INTO "public"."grnti" VALUES ('9', '20', 'Информатика', null);
 
 -- ----------------------------
 -- Table structure for member
@@ -262,8 +287,8 @@ WITH (OIDS=FALSE)
 -- ----------------------------
 -- Records of project
 -- ----------------------------
-INSERT INTO "public"."project" VALUES ('1', 'Облачная Платформа Ведения Реестра Информационных Проектов', '2017-05-24', '', '  ', 'Использование интернет-браузера.', '  ', '');
-INSERT INTO "public"."project" VALUES ('10', 'Второй', '2017-11-22', '', '', '', '', '');
+INSERT INTO "public"."project" VALUES ('1', 'Облачная Платформа Ведения Реестра Информационных Проектов', '2017-05-24', '', 'ПО может быть тиражировано по предприятиям занимающимися инновационными разработками.', 'Использование интернет-браузера.', 'Управление по инновациям ТУСУР.', '');
+INSERT INTO "public"."project" VALUES ('10', 'Геоинформационная система электрокоммуникаций', '2017-11-22', '', '', '', '', '');
 
 -- ----------------------------
 -- Table structure for udk
@@ -285,7 +310,7 @@ WITH (OIDS=FALSE)
 INSERT INTO "public"."udk" VALUES ('9', '004.413', 'Организация разработки программного обеспечения', '14');
 INSERT INTO "public"."udk" VALUES ('12', '004', 'Информационные технологии. Компьютерные технологии. Теория вычислительных машин и систем', null);
 INSERT INTO "public"."udk" VALUES ('13', '004.4', 'Программные средства', '12');
-INSERT INTO "public"."udk" VALUES ('14', '004.41', 'Программотехника. Разработка вычислительных систем', '12');
+INSERT INTO "public"."udk" VALUES ('14', '004.41', 'Программотехника. Разработка вычислительных систем', '13');
 
 -- ----------------------------
 -- Table structure for working
@@ -448,20 +473,20 @@ ALTER TABLE "public"."classificate_1" ADD FOREIGN KEY ("project_id") REFERENCES 
 -- ----------------------------
 -- Foreign Key structure for table "public"."classificate_2"
 -- ----------------------------
-ALTER TABLE "public"."classificate_2" ADD FOREIGN KEY ("project_id") REFERENCES "public"."project" ("project_id") ON DELETE RESTRICT ON UPDATE RESTRICT;
 ALTER TABLE "public"."classificate_2" ADD FOREIGN KEY ("udk_id") REFERENCES "public"."udk" ("udk_id") ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE "public"."classificate_2" ADD FOREIGN KEY ("project_id") REFERENCES "public"."project" ("project_id") ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 -- ----------------------------
 -- Foreign Key structure for table "public"."classificate_3"
 -- ----------------------------
-ALTER TABLE "public"."classificate_3" ADD FOREIGN KEY ("project_id") REFERENCES "public"."project" ("project_id") ON DELETE RESTRICT ON UPDATE RESTRICT;
 ALTER TABLE "public"."classificate_3" ADD FOREIGN KEY ("direction_id") REFERENCES "public"."directions" ("direction_id") ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE "public"."classificate_3" ADD FOREIGN KEY ("project_id") REFERENCES "public"."project" ("project_id") ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 -- ----------------------------
 -- Foreign Key structure for table "public"."collaborator"
 -- ----------------------------
-ALTER TABLE "public"."collaborator" ADD FOREIGN KEY ("project_id") REFERENCES "public"."project" ("project_id") ON DELETE RESTRICT ON UPDATE RESTRICT;
 ALTER TABLE "public"."collaborator" ADD FOREIGN KEY ("member_id") REFERENCES "public"."member" ("member_id") ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE "public"."collaborator" ADD FOREIGN KEY ("project_id") REFERENCES "public"."project" ("project_id") ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 -- ----------------------------
 -- Foreign Key structure for table "public"."working"
